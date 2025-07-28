b25a8dfc25ec38b8b84fdb3dd6e3d026499556e8
emonshikder007 <emonshikder2217@gmail.com>
2025-07-28 20:25:50 +0600

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ mongoose
   .catch((err) => console.log("DB Error:", err));
 
 const corsOptions = {
-  origin: ["http://localhost:1011", "https://your-frontend-domain.com"],
+  origin: ["https://mass-english.onrender.com/", "https://your-frontend-domain.com"],
   credentials: true,
 };
 
